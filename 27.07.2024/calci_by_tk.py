7a0df43bda77e81b88dc1f021ec9d3a97fe2ae8e
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2024-07-27 11:46:18 +0530

    Create calci_by_tk.py

---
+++ 27.07.2024/calci_by_tk.py
@@ -0,0 +1,15 @@
+    import tkinter as tk
+    def evaluate(event):
+        res.configure(text="Result: "+str(eval(entry.get())))
+
+    root=tk.Tk()
+    root.title("Simple Calculator")
+
+    entry=tk.Entry(root)
+    entry.bind("<Return>",evaluate)
+    entry.pack()
+
+    res=tk.Label(root)
+    res.pack()
+
+    root.mainloop()
