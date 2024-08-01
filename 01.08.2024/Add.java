2b2e0fef3abc0748bac9cd053f8ebf5a5924ee4a
343GAUTAMVIJAY <147785180+343GAUTAMVIJAY@users.noreply.github.com>
2024-08-01 21:35:54 +0530

    Create Add.java

---
+++ 01.08.2024/Add.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Add{
+    public static void main(String[] args)
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter item price: ");
+        int a1=sc.nextInt();
+        System.out.println(a1);
+        System.out.print("Enter quantity: ");
+        int b1=sc.nextInt();
+        System.out.println(b1);
+        int sum=a1+b1;
+        System.out.println(sum);
+        
+    }
+}
+
+
+
+
